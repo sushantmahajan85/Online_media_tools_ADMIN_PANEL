9044da2c5bd249d907caf3e710faa1a90e4eb769
sushantmahajan85 <sushantmahajan85@gmail.com>
2024-01-06 16:19:34 +0530

    change admin

--- src/components/Pages/AdminDetailPage.jsx
+++ src/components/Pages/AdminDetailPage.jsx
@@ -3,144 +3,152 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // import { toast } from "react-toastify";
-import style from "./ui.module.css"
+import style from "./ui.module.css";
 import { useSelector } from "react-redux";
-import { selecteUsers } from '../../Store/authSlice';
+import { selecteUsers } from "../../Store/authSlice";
 // import { Loader } from "../Loader/loader";
 
 // const serverURL = process.env.REACT_APP_SERVER_URL
 
-
-
 export function AdminDetailpage() {
+  const storeUser = useSelector(selecteUsers);
+  // const storeAllPosts = useSelector(selectAllPosts)
+  // let [loading, setloading] = useState(false);
+  let [user, setUsers] = useState();
+  // let [Userposts, setUserposts] = useState([]);
 
-    const storeUser = useSelector(selecteUsers)
-    // const storeAllPosts = useSelector(selectAllPosts)
-    // let [loading, setloading] = useState(false);
-    let [user, setUsers] = useState();
-    // let [Userposts, setUserposts] = useState([]);
+  // const [selectedOption, setSelectedOption] = useState(user ? user.status === true ? "Active" : "Suspended" : 'Select..');
 
-    // const [selectedOption, setSelectedOption] = useState(user ? user.status === true ? "Active" : "Suspended" : 'Select..');
+  useEffect(() => {
+    let CurrentUser = storeUser.find((userObject) => {
+      return userObject._id === "658c276082f17d2da1f57c8e";
+    });
 
+    setUsers(CurrentUser);
+  }, [storeUser]);
 
+  // useEffect(() => {
+  //     let CurrentUserPosts = storeAllPosts.filter((post) => {
+  //         return post.userId === id;
+  //     });
 
+  //     setUserposts(CurrentUserPosts);
 
-    useEffect(() => {
+  // }, [storeAllPosts, id])
 
-        let CurrentUser = storeUser.find((userObject) => {
-            return userObject._id === "658c22fc82f17d2da1f57c70";
-        });
+  // // console.log(Userposts);
+  // const handleDropdownChange = (e) => {
+  //     const selectedValue = e.target.value;
 
-        setUsers(CurrentUser)
+  //     setSelectedOption(selectedValue);
 
-    }, [storeUser])
+  // };
 
-    // useEffect(() => {
-    //     let CurrentUserPosts = storeAllPosts.filter((post) => {
-    //         return post.userId === id;
-    //     });
+  // const handelSubmitStatus = async (e) => {
+  //     e.preventDefault()
 
-    //     setUserposts(CurrentUserPosts);
+  //     if (selectedOption === 'Select..') {
+  //         toast.info("Please Select Any Options")
+  //         return
+  //     }
 
-    // }, [storeAllPosts, id])
+  //     try {
 
-    // // console.log(Userposts);
-    // const handleDropdownChange = (e) => {
-    //     const selectedValue = e.target.value;
+  //         setloading(true)
+  //         const response = await axios.post(`${serverURL}/api/users/658c276082f17d2da1f57c8e/update_user_status`, {
+  //             status: selectedOption
+  //         })
+  //         // // console.log(response.data.user);
+  //         if (response && response.status === 200) {
+  //             setloading(false)
+  //             setUsers(response.data.user)
+  //             toast.success(response.data.message)
+  //         }
+  //     } catch (error) {
+  //         setloading(false)
+  //         if (error.response.status === 401) {
+  //             toast.error(error.response.message);
+  //         } else if (error.response.status === 400) {
+  //             toast.error(error.response.message);
+  //         } else if (error.response.status === 500) {
+  //             toast.error(error.response.message);
 
-    //     setSelectedOption(selectedValue);
+  //         } else {
+  //             toast.error("Failed to Update user status")
+  //         }
 
-    // };
+  //     }
+  //     setSelectedOption('Select..')
 
-    // const handelSubmitStatus = async (e) => {
-    //     e.preventDefault()
+  // }
+  return (
+    <>
+      <div className={`p-2  text-light ${style.Sheading} `}>
+        <h2 className={style.Heading}>User Profile</h2>
+      </div>
 
-    //     if (selectedOption === 'Select..') {
-    //         toast.info("Please Select Any Options")
-    //         return
-    //     }
-
-    //     try {
-
-    //         setloading(true)
-    //         const response = await axios.post(`${serverURL}/api/users/658c22fc82f17d2da1f57c70/update_user_status`, {
-    //             status: selectedOption
-    //         })
-    //         // // console.log(response.data.user);
-    //         if (response && response.status === 200) {
-    //             setloading(false)
-    //             setUsers(response.data.user)
-    //             toast.success(response.data.message)
-    //         }
-    //     } catch (error) {
-    //         setloading(false)
-    //         if (error.response.status === 401) {
-    //             toast.error(error.response.message);
-    //         } else if (error.response.status === 400) {
-    //             toast.error(error.response.message);
-    //         } else if (error.response.status === 500) {
-    //             toast.error(error.response.message);
-
-    //         } else {
-    //             toast.error("Failed to Update user status")
-    //         }
-
-    //     }
-    //     setSelectedOption('Select..')
-
-    // }
-    return (<>
-        <div className={`p-2  text-light ${style.Sheading} `}>
-
-            <h2 className={style.Heading}>
-                User Profile
-            </h2>
-        </div>
-
-        {user && <div>
-            <div className="p-3 ">
-                <div className={`${style.ProfileDIv} col-span-2`}>
-                    <div style={{ width: "100%" }} className="d-flex   gap-2 justify-content-between px-4">
-
-                        <div >
-                            <p>
-                                <span className="fw-bold">Name</span> : <span>{user.firstName + " " + user.lastName}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">Mobile Number</span> : <span>{user.mobileNumber}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">UserId</span> : <span>{user._id}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">Designation</span> : <span>{user.Designation}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">About </span> : <span>{user.AboutMe}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">Facebook</span> : <span>{user.Facebook}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">Instagram</span> : <span>{user.Instagram}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">LinkedIn</span> : <span>{user.LinkedIn}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">Skypa</span> : <span>{user.Skypa}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">Telegram</span> : <span>{user.Telegram}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold">email</span> : <span>{user.email}</span>
-                            </p>
-                            <p>
-                                <span className="fw-bold" >Account</span> :  {user.isverified === true ? <span className="text-success fw-bolder">Verified</span> : <span className="text-danger fw-bolder ">Unverified</span>}
-
-                            </p>
-                            {/* <p>
+      {user && (
+        <div>
+          <div className="p-3 ">
+            <div className={`${style.ProfileDIv} col-span-2`}>
+              <div
+                style={{ width: "100%" }}
+                className="d-flex   gap-2 justify-content-between px-4"
+              >
+                <div>
+                  <p>
+                    <span className="fw-bold">Name</span> :{" "}
+                    <span>{user.firstName + " " + user.lastName}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">Mobile Number</span> :{" "}
+                    <span>{user.mobileNumber}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">UserId</span> :{" "}
+                    <span>{user._id}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">Designation</span> :{" "}
+                    <span>{user.Designation}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">About </span> :{" "}
+                    <span>{user.AboutMe}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">Facebook</span> :{" "}
+                    <span>{user.Facebook}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">Instagram</span> :{" "}
+                    <span>{user.Instagram}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">LinkedIn</span> :{" "}
+                    <span>{user.LinkedIn}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">Skypa</span> :{" "}
+                    <span>{user.Skypa}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">Telegram</span> :{" "}
+                    <span>{user.Telegram}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">email</span> :{" "}
+                    <span>{user.email}</span>
+                  </p>
+                  <p>
+                    <span className="fw-bold">Account</span> :{" "}
+                    {user.isverified === true ? (
+                      <span className="text-success fw-bolder">Verified</span>
+                    ) : (
+                      <span className="text-danger fw-bolder ">Unverified</span>
+                    )}
+                  </p>
+                  {/* <p>
                                 <span className="fw-bold">Status</span> :  {user.status === true ? <span className={style.active}>Active</span> : <span className={style.suspend}>Suspended</span>}
 
                             </p>
@@ -166,39 +174,56 @@ export function AdminDetailpage() {
                                 </form>
 
                             </div> */}
-                        </div>
-                        <div  >
-                            <img src={user.profileImageUrl} alt="profileImage" width={"120rem"} height={"120rem"} style={{ borderRadius: "1rem" }} />
-                        </div>
-                    </div>
                 </div>
+                <div>
+                  <img
+                    src={user.profileImageUrl}
+                    alt="profileImage"
+                    width={"120rem"}
+                    height={"120rem"}
+                    style={{ borderRadius: "1rem" }}
+                  />
+                </div>
+              </div>
             </div>
-        </div>}
-
-        <div className="container">
-            <div className="row gap-2  my-4 px-4 text-center">
-                <Link to={`/Admin/AdminDashboard/UserDetails/658c22fc82f17d2da1f57c70/UserChats`} className={`col ${style.Box}`}>
-                    <div>
-                        Chats
-                    </div>
-                    <div>
-                        <img src="/chats.png" alt="chatimg" width={"30rem"} height={"30rem"} />
-                    </div>
-                </Link>
-                <Link to={`/Admin/AdminDashboard/UserDetails/658c22fc82f17d2da1f57c70/Posts`} className={`col ${style.Box}`}>
-                    <div>
-                        Posts
-                    </div>
-                    <div>
-                        <img src="/mpost.png" alt="chatimg" width={"30rem"} height={"30rem"} />
-                    </div>
-                </Link>
-            </div>
+          </div>
         </div>
+      )}
 
+      <div className="container">
+        <div className="row gap-2  my-4 px-4 text-center">
+          <Link
+            to={`/Admin/AdminDashboard/UserDetails/658c276082f17d2da1f57c8e/UserChats`}
+            className={`col ${style.Box}`}
+          >
+            <div>Chats</div>
+            <div>
+              <img
+                src="/chats.png"
+                alt="chatimg"
+                width={"30rem"}
+                height={"30rem"}
+              />
+            </div>
+          </Link>
+          <Link
+            to={`/Admin/AdminDashboard/UserDetails/658c276082f17d2da1f57c8e/Posts`}
+            className={`col ${style.Box}`}
+          >
+            <div>Posts</div>
+            <div>
+              <img
+                src="/mpost.png"
+                alt="chatimg"
+                width={"30rem"}
+                height={"30rem"}
+              />
+            </div>
+          </Link>
+        </div>
+      </div>
 
-
-        {/* <Loader loading={loading} /> */}
-
-    </>)
+      {/* <Loader loading={loading} /> */}
+    </>
+  );
 }
--- src/components/Pages/usertables.js
+++ src/components/Pages/usertables.js
@@ -2,34 +2,31 @@ import { CardBody, Table, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import React from "react";
-import 'react-dropdown/style.css';
-import style from "./ui.module.css"
+import "react-dropdown/style.css";
+import style from "./ui.module.css";
 // import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { selecteUsers } from "../../Store/authSlice";
 import { DeleteModel } from "./DeleteModel";
 
-
 const ProjectTables = () => {
-
-  const storeUsers = useSelector(selecteUsers)
+  const storeUsers = useSelector(selecteUsers);
   // console.log(storeUsers);
-  const [deltedId, setDeletedId] = useState()
-  const [deleteWhatUsers, setdeleteWhatUsers] = useState('')
-  const [pContent, setpContent] = useState()
+  const [deltedId, setDeletedId] = useState();
+  const [deleteWhatUsers, setdeleteWhatUsers] = useState("");
+  const [pContent, setpContent] = useState();
 
   const [currentData, setcurrentData] = useState();
   // const [selectedOption, setSelectedOption] = useState('Select..');
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
 
-
-
-
   useEffect(() => {
-    const alluser = storeUsers.filter((user) => user._id !== "658c22fc82f17d2da1f57c70")
-    setcurrentData(alluser)
-  }, [storeUsers])
+    const alluser = storeUsers.filter(
+      (user) => user._id !== "658c276082f17d2da1f57c8e"
+    );
+    setcurrentData(alluser);
+  }, [storeUsers]);
 
   // const handleDropdownChange = (e) => {
   //   const selectedValue = e.target.value;
@@ -61,45 +58,42 @@ const ProjectTables = () => {
 
   // };
 
-
-
-
-
-  return (<div >
-
-    {currentData && <div>
-      <div className={`p-2  text-light ${style.Sheading} `}>
-
-        <h2 className={style.Heading}>
-          Users
-        </h2>
-      </div>
-
-      <div>
-        <CardBody>
-          <div className="d-flex align-items-center justify-content-between  ">
-
-            <div>
-              <h2 tag="h5">  User Listing</h2>
-              <p className="text-muted" tag="h6">
-                Overview of the All Users
-              </p>
-            </div>
-
-            <div>
-              <button onClick={() => {
-                setModal(!modal);
-                setpContent(" Are you sure you want to Delete All Unverified Users? This action cannot be undone.")
-                setdeleteWhatUsers("UnverifiedUsers")
-              }} className="bg-danger text-white p-2 fw-bold border-0 rounded ">
-                Delete All Unverified Users
-              </button>
-            </div>
+  return (
+    <div>
+      {currentData && (
+        <div>
+          <div className={`p-2  text-light ${style.Sheading} `}>
+            <h2 className={style.Heading}>Users</h2>
           </div>
 
+          <div>
+            <CardBody>
+              <div className="d-flex align-items-center justify-content-between  ">
+                <div>
+                  <h2 tag="h5"> User Listing</h2>
+                  <p className="text-muted" tag="h6">
+                    Overview of the All Users
+                  </p>
+                </div>
 
-          <div className={style.inputDivMain}>
-            {/* <div >
+                <div>
+                  <button
+                    onClick={() => {
+                      setModal(!modal);
+                      setpContent(
+                        " Are you sure you want to Delete All Unverified Users? This action cannot be undone."
+                      );
+                      setdeleteWhatUsers("UnverifiedUsers");
+                    }}
+                    className="bg-danger text-white p-2 fw-bold border-0 rounded "
+                  >
+                    Delete All Unverified Users
+                  </button>
+                </div>
+              </div>
+
+              <div className={style.inputDivMain}>
+                {/* <div >
               <label style={{ fontWeight: "bold", marginRight: "10px" }} htmlFor="userStatus">Select User Status:</label>
               <select
                 className={style.dropdown}
@@ -116,7 +110,7 @@ const ProjectTables = () => {
               </select>
             </div> */}
 
-            {/* <form className={style.inputDiv}>
+                {/* <form className={style.inputDiv}>
               <input ref={searchInputRef} value={searchvalue} onChange={handelSearchinput} placeholder="Search User" className={style.SearchInput} type="search" name="" id="" />
 
               <button type="submit" onClick={searchUsers} >
@@ -126,71 +120,94 @@ const ProjectTables = () => {
                 </svg>
               </button>
             </form> */}
-          </div>
-          <Table className="no-wrap  align-middle" responsive borderless>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Contact</th>
-                <th>Designation</th>
-                <th>Account</th>
-                {/* <th>Status</th> */}
-                <th>Action</th>
-
-              </tr>
-            </thead>
-            <tbody>
-              {currentData.map((tdata, index) => (
-                <tr key={index} className="border-top">
-                  <td>
-                    <Link to={`/Admin/AdminDashboard/UserDetails/${tdata._id}`} className={`${style.userProfile} d-flex align-items-center text-dark pointer p-2`}>
-                      {tdata.profileImageUrl &&
-                        <img
-                          src={tdata.profileImageUrl}
-                          className="rounded-circle"
-                          alt="avatar"
-                          width="45"
-                          height="45"
-                        />
-                      }
-                      <h6 className="mb-0 mx-2">{tdata.firstName ? (tdata.firstName + " " + tdata.lastName) : 'NaN'}</h6>
-
-                    </Link>
-                  </td>
-                  <td>{tdata.email ? tdata.email : 'NaN'}</td>
-                  <td>{tdata.mobileNumber ? tdata.mobileNumber : "NaN"}</td>
-                  <td>{tdata.Designation ? tdata.Designation : 'NaN'}</td>
-                  <td>
-                    {tdata.isverified === true ? <span className="text-success fw-bolder">Verified</span> : <span className="text-danger fw-bolder ">Unverified</span>}
-                  </td>
-                  {/* <td>
+              </div>
+              <Table className="no-wrap  align-middle" responsive borderless>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Contact</th>
+                    <th>Designation</th>
+                    <th>Account</th>
+                    {/* <th>Status</th> */}
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {currentData.map((tdata, index) => (
+                    <tr key={index} className="border-top">
+                      <td>
+                        <Link
+                          to={`/Admin/AdminDashboard/UserDetails/${tdata._id}`}
+                          className={`${style.userProfile} d-flex align-items-center text-dark pointer p-2`}
+                        >
+                          {tdata.profileImageUrl && (
+                            <img
+                              src={tdata.profileImageUrl}
+                              className="rounded-circle"
+                              alt="avatar"
+                              width="45"
+                              height="45"
+                            />
+                          )}
+                          <h6 className="mb-0 mx-2">
+                            {tdata.firstName
+                              ? tdata.firstName + " " + tdata.lastName
+                              : "NaN"}
+                          </h6>
+                        </Link>
+                      </td>
+                      <td>{tdata.email ? tdata.email : "NaN"}</td>
+                      <td>{tdata.mobileNumber ? tdata.mobileNumber : "NaN"}</td>
+                      <td>{tdata.Designation ? tdata.Designation : "NaN"}</td>
+                      <td>
+                        {tdata.isverified === true ? (
+                          <span className="text-success fw-bolder">
+                            Verified
+                          </span>
+                        ) : (
+                          <span className="text-danger fw-bolder ">
+                            Unverified
+                          </span>
+                        )}
+                      </td>
+                      {/* <td>
                     {tdata.status === true ? <button className={style.active}>Active</button> : <button className={style.suspend}>Suspended</button>}
                   </td> */}
-                  <td>
-                    <Button className="Reject"
-                      onClick={() => {
-                        setDeletedId(tdata._id)
-                        setModal(!modal);
-                        setdeleteWhatUsers("user")
-                        setpContent(' Are you sure you want to Delete  your account? All of your data will be permanently removed. This action cannot be undone.')
-                      }}
-                    ><i className="bi bi-trash3"></i></Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
+                      <td>
+                        <Button
+                          className="Reject"
+                          onClick={() => {
+                            setDeletedId(tdata._id);
+                            setModal(!modal);
+                            setdeleteWhatUsers("user");
+                            setpContent(
+                              " Are you sure you want to Delete  your account? All of your data will be permanently removed. This action cannot be undone."
+                            );
+                          }}
+                        >
+                          <i className="bi bi-trash3"></i>
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </CardBody>
+          </div>
+        </div>
+      )}
 
-        </CardBody>
-      </div>
-    </div>}
-
-
-
-    <DeleteModel modal={modal} setModal={setModal} toggle={toggle} pContent={pContent} deleteWhat={deleteWhatUsers} deltedId={deltedId} />
-  </div>);
+      <DeleteModel
+        modal={modal}
+        setModal={setModal}
+        toggle={toggle}
+        pContent={pContent}
+        deleteWhat={deleteWhatUsers}
+        deltedId={deltedId}
+      />
+    </div>
+  );
 };
 
 export default ProjectTables;
-
